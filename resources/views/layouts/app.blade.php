3d03a9d1c32d2d43539b3fce1f225cab7a0c0ecf
AymanKUMA <aymane.kamel@edu.uca.ma>
2022-07-29 10:49:33 +0100

    logo&routeModification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,71 +20,130 @@
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-    <!--slider js library-->
-    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,700;1,700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ url('css/sideBar.css') }}" rel="stylesheet">
 </head>
 
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+    <div id="wrapper">
+        <!-- Sidebar -->
+        <div class="navbar-nav  sidebar sidebar-dark accordion" id="accordionSidebar">
+            <!-- Sidebar - Brand -->
+            <div style="padding: 10px 0">
+                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+                    <div class="sidebar-brand-icon ">
+                        <img src={{url('images/logo_white.svg')}} alt="" width="175">
+                    </div>
+
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                <!-- Divider -->
+                <hr class="sidebar-divider d-none d-md-block mb-5">
+            </div>
 
-                    </ul>
+            <ul style="list-style: none; padding: 0 ">
+                <li class="nav-item">
+                    <a class="nav-link  active w-100" href="#">
+                        <i class="fa-solid fa-images "></i>
+                        <span>Sliders</span>
+                    </a>
+                </li>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                <li class="nav-item ">
+                    <a class="nav-link  w-100" href="alerts.html">
+                        <i class="fa-solid fa-triangle-exclamation "></i>
+                        <span>Alerts</span>
+                    </a>
+                </li>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                <li class="nav-item ">
+                    <a class="nav-link w-100" href="#">
+                        <i class="fa-solid fa-eye "></i>
+                        <span>View</span></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link w-100" href="#">
+                        <i class="fa-solid fa-house "></i>
+                        <span>Home</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- End of Sidebar -->
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav me-auto">
+
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ms-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                @if (Route::has('login'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
 
-        <main class="py-4">
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
             @yield('content')
-        </main>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"
+        integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
+        integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous">
+    </script>
+    <script src="https://kit.fontawesome.com/85c9736486.js" crossorigin="anonymous"></script>
+    <script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js"></script>
+
 </body>
 
 </html>
--- resources/views/slides/index.blade.php
+++ resources/views/slides/index.blade.php
@@ -1,5 +1,47 @@
+@extends('layouts.app')
+
 @section('title', 'Slides')
 
 @section('content')
-    
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="p-3 pb-2">
+
+        <form class="d-flex justify-content-center m-auto mt-5" role="search" style="max-width: 600px">
+            <input class="form-control me-2 " type="search" placeholder="Search" aria-label="Search">
+            <button class="button" type="submit" name="find">Search</button>
+        </form>
+        <div class="box-container">
+            <div class="box-heading row">
+
+                <div class="col">Title</div>
+                <div class="col">Text position</div>
+                <div class="col">Update by</div>
+                <div class="col">Last Update</div>
+
+                <div class="col">
+                    <a href="add-slider.html" class="button add-colors">
+                        <i class="fa-solid fa-plus"></i> Add slider
+                    </a>
+                </div>
+            </div>
+            <div class="box-divider"></div>
+            <div class="box box-dark row">
+
+                <div class="col">Welcome</div>
+                <div class="col">Right</div>
+                <div class="col">SUPER_ADMIN</div>
+                <div class="col">2022-07-28</div>
+                <div class="col ">
+                    <button type="button" class="button update-colors">
+                        <i class="fa-solid fa-pencil"></i>
+                    </button>
+                    <button type="button" class="button delete-colors">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- End of Content Wrapper -->
 @endsection
