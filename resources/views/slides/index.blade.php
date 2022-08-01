f9d75df04b0367386e60b90f913212ceb2642f11
AymanKUMA <aymane.kamel@edu.uca.ma>
2022-08-01 13:25:14 +0100

    table

--- resources/views/slides/index.blade.php
+++ resources/views/slides/index.blade.php
@@ -3,6 +3,92 @@
 @section('title', 'Slides')
 
 @section('content')
+
+<div class="container py-4 position-relative" aria-live="polite" aria-atomic="true" style="height: calc(100vh - 70px)">
+    <div class="toast-container bottom-0 start-0 p-3 ">
+
+        <!-- Then put toasts within -->
+        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <img src="/images/logo.svg" class="rounded me-auto" alt="Reminex">
+
+                <small class="text-muted">just now</small>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast"
+                        aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                See? Just like this.
+            </div>
+        </div>
+
+        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <img src="/images/logo.svg" class="rounded me-auto" alt="Reminex">
+
+                <small class="text-muted">2 seconds ago</small>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast"
+                        aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                Heads up, toasts will stack automatically
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 p-3 d-flex justify-content-start align-items-center">
+            <a href={{ route('slides.create') }} class="btn btn-success">
+                <i class="fa-solid fa-plus"></i> Add slider
+            </a>
+        </div>
+        <div class="col-sm-6 p-3 d-flex justify-content-end align-items-center">
+            <label for="find" class="form-label mb-0 me-1">Search:</label>
+            <input id="find" class="form-control me-2 " type="search" style="max-width: 350px"
+                   placeholder="search" aria-label="Search" oninput="findInTable(this.value,'sortTab')">
+        </div>
+
+    </div>
+
+    <table id="sortTab" class="table table-striped table-dark table-bordered nowrap rounded-3">
+        <thead>
+        <tr>
+            <th role="button" onclick="sortTable(0)"> Titre <i class="fa-solid fa-sort fa-2xs"></i></th>
+            <th role="button" onclick="sortTable(1)" style="width: 150px"> Image Position <i
+                    class="fa-solid fa-sort fa-2xs"></i></th>
+            <th role="button" onclick="sortTable(2)" style="width: 120px">last Update <i
+                    class="fa-solid fa-sort fa-2xs"></i></th>
+            <th role="button" onclick="sortTable(3)"> Update by <i class="fa-solid fa-sort fa-2xs"></i></th>
+            <th role="button" onclick="sortTable(4)" style="width: 120px"> Create at <i
+                    class="fa-solid fa-sort fa-2xs"></i></th>
+            <th role="button" onclick="sortTable(5)"> Create by <i class="fa-solid fa-sort fa-2xs"></i></th>
+            <th class="text-center" style="width: 150px">
+                Actions
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Welcome</td>
+            <td>Right</td>
+            <td>2022-07-31</td>
+            <td>Mehdi Moulati</td>
+            <td>2022-07-30</td>
+            <td>Mehdi Moulati</td>
+            <td class="">
+                <button type="button" class="btn btn-primary btn-sm mx-2">
+                    <i class="fa-solid fa-pencil"></i>
+                </button>
+                <button type="button" class="btn btn-danger btn-sm mx-2">
+                    <i class="fa-solid fa-trash-can"></i>
+                </button>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
+
+</div>
+
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="p-3 pb-2">
 
