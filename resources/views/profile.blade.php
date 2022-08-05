af8d9a3039a36fddfc58ce77b731a76ce7eea0c2
Store <moulati.mehdi@gmail.com>
2022-08-05 23:47:13 +0100

    view&edit profile in same page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,7 +10,7 @@
                     <div class="col col-auto py-1 d-flex align-items-center flex-column ">
                         <div class="fw-bold text-center text-dark fs-5">{{ ucfirst(Auth::user()->name) }}</div>
                         <div class="fw-bold text-center text-muted mb-3" style="font-size: small">{{ Auth::user()->email }}</div>
-                        <img  class="rounded-circle border border-5 border-warning" style="width: 125px !important; height: 125px !important;" src="https://github.com/mdo.png"></img>
+                        <img  class="rounded-circle border border-5 border-warning mb-3" style="width: 110px !important; height: 110px !important;" src="https://github.com/mdo.png"></img>
                     </div>
                 </div>
                 <div class="row align-items-center justify-content-center pb-4">
@@ -27,38 +27,45 @@
                 <h6 class="text-muted p-3" style="font-size:14px;"><b>inscrit depuis</b>: {{ Auth::user()->created_at->toFormattedDateString() }}</h6>
 
             </div>
-            <form class="card col-md-5 border-right p-4" method="POST" action="{{ route('updateProfile')}}">
-                @csrf
-                @method('PUT')
+            <div class="col col-md-5">
+                <form class="card " method="POST" action="{{ route('updateProfile')}}">
+                    <div class="card-header text-center p-3">
+                        Profile Settings
+                    </div>
+                    @csrf
+                    @method('PUT')
 
 
-                <div class="p-3 py-5">
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h4 class="text-right" style="color: var(--main-color);">Profile Settings</h4>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-12">
-                            <label class="labels">Name</label>
-                            <input type="text" class="form-control" placeholder="full name" value="{{ Auth::user()->name }}">
+                    <div class="card-body p-5">
+                        {{--                    <div class="d-flex justify-content-between align-items-center mb-3">--}}
+                        {{--                        <h4 class="text-right" style="color: var(--main-color);">Profile Settings</h4>--}}
+                        {{--                    </div>--}}
+                        <div class="row mt-3">
+                            <div class="col-md-12">
+                                <label class="labels">Name</label>
+                                <input type="text" class="form-control" placeholder="full name" value="{{ Auth::user()->name }}">
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-12">
+                                <label class="labels">Username</label>
+                                <input type="text" class="form-control" placeholder="enter a username" value="{{ Auth::user()->username }}">
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-12">
+                                <label class="labels">Email</label>
+                                <input type="text" class="form-control" placeholder="enter an email" value="{{ Auth::user()->email }}">
+                            </div>
                         </div>
                     </div>
-                    <div class="row mt-3">
-                        <div class="col-md-12">
-                            <label class="labels">Username</label>
-                            <input type="text" class="form-control" placeholder="enter a username" value="{{ Auth::user()->username }}">
-                        </div>
+                    <div class="card-footer d-flex justify-content-end p-3">
+                        <button type="submit" class="btn btn-warning profile-button ">Edit profile</button>
                     </div>
-                    <div class="row mt-3">
-                        <div class="col-md-12">
-                            <label class="labels">Email</label>
-                            <input type="text" class="form-control" placeholder="enter an email" value="{{ Auth::user()->email }}">
-                        </div>
-                    </div>
-                </div>
-                <div class="d-flex justify-content-end mb-3">
-                        <button type="submit" class="btn btn-warning profile-button">Edit profile</button>
-                </div>
-            </form>
+
+                </form>
+            </div>
+
         </div>
 
 @endsection
