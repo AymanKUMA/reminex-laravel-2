2c48895f88eb50f100d1c7f0003b195dbe49f67e
Store <moulati.mehdi@gmail.com>
2022-08-08 12:00:45 +0100

    store user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
@@ -15,8 +16,8 @@ class UsersController extends Controller
     public function index()
     {
         //
-        return view('users.index',[
-            'users'=>User::all()
+        return view('users.index', [
+            'users' => User::all()
         ]);
     }
 
@@ -34,24 +35,37 @@ class UsersController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'name' => 'required',
+            'username' => 'required',
+            'email' => 'required',
+            'isadmin' => 'required|integer',
+            'password' => 'required',
+            'image' => 'mimes:jpg,png,svg,jpeg|max:6000'
+        ]);
+
+
+        $user = $this->prepareUser($request);
+        $user->save();
+        return redirect()->route('users.index')->with('message', 'User added successfully !');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($user)
     {
         //
-        return view('users.show',[
+        return view('users.show', [
             'user' => User::findOrFail($user),
         ]);
     }
@@ -59,7 +73,7 @@ class UsersController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($user)
@@ -70,8 +84,8 @@ class UsersController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $user)
@@ -82,11 +96,25 @@ class UsersController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($user)
     {
         //
+    }
+
+    public function prepareUser($request)
+    {
+        $user = new User();
+
+        $user->name = $request->name;
+        $user->username = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->image = time() . '-' . str_replace(' ', '', $request->title) . '.' . $request->image->extension();
+
+        $request->image->move(public_path('slides_images'), $user->image);
+        return $user;
     }
 }
