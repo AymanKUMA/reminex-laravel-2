beac616ce93986d368db8564bfd850d3faeb0ffd
AymanKUMA <aymane.kamel@edu.uca.ma>
2022-08-09 12:14:08 +0100

    error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,8 +5,9 @@
 @section('content')
     <div class="d-flex justify-content-center align-items-center flex-column" style="height: calc(100vh - 80px)">
             <img src="{{ url('images/page-not-found.svg')}}" width="350" alt="">
-        <h2 class="text-center mt-4 mb-2"> Sorry</h2>
+        <p class="text-center mt-4" style="color: orange; font-size: 100px;">ERROR 404</p>
+        <h3 class="text-center mb-2"> Sorry</h3>
         <h5 class=" text-center mb-4 text-muted"> We can't Find the Page that you're looking for</h5>
-        <a class="btn btn-primary" href="{{ route('welcomePage') }}">Return To Home Page</a>
+        <a class="btn btn-warning" href="{{ route('welcomePage') }}">Return To Home Page</a>
     </div>
 @endsection
