98f60b76a285fdd747870d09fa618b18a6be89a7
Store <moulati.mehdi@gmail.com>
2022-08-11 13:27:34 +0100

    create from edit fontend

--- resources/views/slides/create.blade.php
+++ resources/views/slides/create.blade.php
@@ -4,63 +4,13 @@
 
 @section('content')
     <!-- Content Wrapper -->
-    <div id="content-wrapper" class="p-3 pb-2">
+    <div id="content-wrapper" class="container">
 
-        <form method="POST" action="{{route('slides.store')}}" class="container col col-md-6 " enctype="multipart/form-data">
+        <form method="POST" action="{{route('slides.store')}}" class="card text-bg-light " enctype="multipart/form-data">
             @csrf
-            <h1 style="color: var(--main-color);">Slides</h1>
-            <div class="row row-cols-2 justify-content-center align-items-center">
-                <div class="col col-md-6 col-12 mb-3">
-                    <label for="title" class="form-label">Titre : <span style="color: red">*</span> </label>
-                    <input type="text" name="title" placeholder="Titre" id="titre" class="form-control" required>
-                </div>
-                <div class="col col-md-6 col-12 mb-3">
-                    <label for="subtitle" class="form-label">Sous-titre : <span style="color: red">*</span></label>
-                    <input type="text" name="subtitle" placeholder="Sous-Titre" id="subtitre" class="form-control"
-                        required>
-                </div>
-                <div class="col col-12 mb-3">
-                    <label for="description" class="form-label">
-                        Description
-                        <span style="color: red">*</span> :
-                    </label>
-                    <textarea name="description" placeholder="Description" id="description" class="form-control" rows="2" maxlength="400" required></textarea>
-                </div>
-                <div class="col col-12 mb-3">
-                    <label class="form-label d-inline-block mb-3">Position de texte :</label>
-                    <div class="row row-cols-2">
-
-                        <div class="col ">
-                            <div class="row justify-content-center">
-                                <img src={{ url('/images/left.svg') }} alt="gauche" style="width: 200px">
-                            </div>
-                            <div class="row m-3">
-                                <label class="form-check-label" style="text-align: center">
-                                    Gauche
-                                    <input type="radio" name="layout" class="form-check-input"
-                                        value="left">
-                                </label>
-                            </div>
-                        </div>
-                        <div class="col ">
-                            <div class="row justify-content-center">
-                                <img src={{ url('/images/right.svg') }} alt="right" style="width: 200px">
-                            </div>
-                            <div class="row m-3">
-                                <label style="text-align: center" class="form-check-label">
-                                    Droite
-                                    <input type="radio" name="layout" class="form-check-input"
-                                        value="right" checked>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
-                <div class="col col-12 mb-3 ">
-
-
+            <h3 class="card-header p-3 bg-dark rounded-2" style="color: var(--main-color);">Slides</h3>
+            <div class="card-body p-4 row row-cols-2 justify-content-around align-items-center">
+                <div class="col col-5">
                     <label class="form-label">
                         Image <span style="color: red">* </span> :
                     </label>
@@ -68,7 +18,7 @@
                         <img src="#" id="chosenImg" class="img-fluid rounded-4" alt="">
 
                         <label for="image" id="add-image"
-                            class="d-flex flex-column justify-content-center align-items-center">
+                               class="d-flex flex-column justify-content-center align-items-center">
 
                             <img role="button" src="{{ url('/images/add-image.svg') }}" alt="add-image">
                             <span>Importer un image</span>
@@ -76,7 +26,7 @@
                         </label>
 
                         <input type="file" name="image" placeholder="Titre" id="image" class="form-control" hidden
-                            onchange="readURL(this)" required>
+                               onchange="readURL(this)" required>
                     </div>
                     <script>
                         function readURL(input) {
@@ -95,13 +45,68 @@
 
 
                 </div>
+                <div class="col col-5">
+                    <div class="row row-cols-2">
+                        <div class="col col-md-6 col-12 mb-3">
+                            <label for="title" class="form-label">Titre : <span style="color: red">*</span> </label>
+                            <input type="text" name="title" placeholder="Titre" id="titre" class="form-control" required>
+                        </div>
+                        <div class="col col-md-6 col-12 mb-3">
+                            <label for="subtitle" class="form-label">Sous-titre : <span style="color: red">*</span></label>
+                            <input type="text" name="subtitle" placeholder="Sous-Titre" id="subtitre" class="form-control"
+                                   required>
+                        </div>
+                        <div class="col col-12 mb-3">
+                            <label for="description" class="form-label">
+                                Description
+                                <span style="color: red">*</span> :
+                            </label>
+                            <textarea name="description" placeholder="Description" id="description" class="form-control" rows="2" maxlength="400" required></textarea>
+                        </div>
+                        <div class="col col-12 mb-3">
+                            <label class="form-label d-inline-block mb-3">Position de texte :</label>
+                            <div class="row row-cols-2">
+
+                                <div class="col ">
+                                    <div class="row justify-content-center">
+                                        <img src={{ url('/images/left.svg') }} alt="gauche" style="width: 200px" alt="">
+                                    </div>
+                                    <div class="row m-3">
+                                        <label class="form-check-label" style="text-align: center">
+                                            Gauche
+                                            <input type="radio" name="layout" class="form-check-input"
+                                                   value="left">
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col ">
+                                    <div class="row justify-content-center">
+                                        <img src={{ url('/images/right.svg') }} alt="right" style="width: 200px" alt="">
+                                    </div>
+                                    <div class="row m-3">
+                                        <label style="text-align: center" class="form-check-label">
+                                            Droite
+                                            <input type="radio" name="layout" class="form-check-input"
+                                                   value="right" checked>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
 
 
             </div>
-            <div class="d-flex justify-content-end mb-3">
-                <input class="button" type="submit" name="addSlide" value="ajouter">
-            </div>
 
+            <div class="card-footer">
+                <div class="d-flex justify-content-end">
+                    <input class="button" type="submit" name="addSlide" value="ajouter">
+                </div>
+            </div>
         </form>
     </div>
 @endsection
